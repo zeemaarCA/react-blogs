f62066e1e500f00be7b32750650e1ee6674876f7
zeemaarCA <zeemaarca@gmail.com>
2024-04-15 19:41:47 +0500

    check other method for equal query

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -81,16 +81,18 @@ export class Service {
         }
     }
 
-    async getPosts(queries = [Query.equal("status", "active")]) {
+    // async getPosts(queries = [Query.equal("status", "active")]) {
+    async getPosts() {
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                queries,
-            )
+                // queries,
+                [Query.equal("status", "active")]
+            );
         } catch (error) {
             console.log("Appwrite serive :: getPosts :: error", error);
-            return false
+            return false;
         }
     }
 
